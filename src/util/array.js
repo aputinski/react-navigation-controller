babae42b5a60c47a01630a11b9f97e30a0f54108
Adam Putinski <adam.putinski@gmail.com>
2015-03-30 03:18:48 -0400

    License

--- src/util/array.js
+++ src/util/array.js
@@ -1,3 +1,21 @@
+/*!
+ * Adapted from LoDash
+ *
+ * https://lodash.com/
+ *
+ * Copyright 2012-2015 The Dojo Foundation <http://dojofoundation.org/>
+ * Based on Underscore.js, copyright 2009-2015 Jeremy Ashkenas,
+ * DocumentCloud and Investigative Reporters & Editors <http://underscorejs.org/>
+
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ */
+
 /**
  * Creates a slice of `array` with `n` elements dropped from the end.
  *
