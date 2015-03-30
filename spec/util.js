f2ae1054c02dcaf27279c308337d6241f3f6dc45
Adam Putinski <adam.putinski@gmail.com>
2015-03-29 23:24:38 -0400

    `assign()` util

--- spec/util.js
+++ spec/util.js
@@ -3,55 +3,72 @@ const {
   dropRight,
   last,
   takeRight,
-  capitalize
+  capitalize,
+  assign
 } = require('../src/util');
 
 describe('Util', () => {
-  describe('#getVendorPrefix', () => {
-    let el;
-    it('gets the correct prefix', () => {
-      el = {style:{'transform':true,'webkitTransform':true}};
-      expect(getVendorPrefix('transform', el)).to.equal('transform');
-      el = {style:{'webkitTransform':true}};
-      expect(getVendorPrefix('transform', el)).to.equal('webkitTransform');
+  describe('Misc', () => {
+    describe('#getVendorPrefix', () => {
+      let el;
+      it('gets the correct prefix', () => {
+        el = {style:{'transform':true,'webkitTransform':true}};
+        expect(getVendorPrefix('transform', el)).to.equal('transform');
+        el = {style:{'webkitTransform':true}};
+        expect(getVendorPrefix('transform', el)).to.equal('webkitTransform');
+      });
     });
   });
-  describe('#dropRight', () => {
-    it('drops the last element from the array', () => {
-      const a = dropRight([1,2,3,4,5,6,7,8,9,10]);
-      expect(a).to.have.length(9);
-      expect(a[8]).to.equal(9);
+  describe('Array', () => {
+    describe('#dropRight', () => {
+      it('drops the last element from the array', () => {
+        const a = dropRight([1,2,3,4,5,6,7,8,9,10]);
+        expect(a).to.have.length(9);
+        expect(a[8]).to.equal(9);
+      });
+      it('drops the specified number of elements from the end of the array', () => {
+        const a = dropRight([1,2,3,4,5,6,7,8,9,10], 2);
+        expect(a).to.have.length(8);
+        expect(a[7]).to.equal(8);
+      });
     });
-    it('drops the specified number of elements from the end of the array', () => {
-      const a = dropRight([1,2,3,4,5,6,7,8,9,10], 2);
-      expect(a).to.have.length(8);
-      expect(a[7]).to.equal(8);
+    describe('#last', () => {
+      it('gets the last element in the array', () => {
+        expect(last([1,2,3])).to.equal(3);
+      });
+    });
+    describe('#takeRight', () => {
+      it('get the last element from the array', () => {
+        const a = takeRight([1,2,3,4,5,6,7,8,9,10]);
+        expect(a).to.have.length(1);
+        expect(a[0]).to.equal(10);
+      });
+      it('gets the specified number of elements from the end of the array', () => {
+        const a = takeRight([1,2,3,4,5,6,7,8,9,10], 3);
+        expect(a).to.have.length(3);
+        expect(a[0]).to.equal(8);
+        expect(a[1]).to.equal(9);
+        expect(a[2]).to.equal(10);
+      });
     });
   });
-  describe('#last', () => {
-    it('gets the last element in the array', () => {
-      expect(last([1,2,3])).to.equal(3);
+  describe('String', () => {
+    describe('#capitalize', () => {
+      it('capitalizes the first character of a word', () => {
+        expect(capitalize('hello')).to.equal('Hello');
+        expect(capitalize('Hello')).to.equal('Hello');
+        expect(capitalize('hello world')).to.equal('Hello world');
+      });
     });
   });
-  describe('#takeRight', () => {
-    it('get the last element from the array', () => {
-      const a = takeRight([1,2,3,4,5,6,7,8,9,10]);
-      expect(a).to.have.length(1);
-      expect(a[0]).to.equal(10);
-    });
-    it('gets the specified number of elements from the end of the array', () => {
-      const a = takeRight([1,2,3,4,5,6,7,8,9,10], 3);
-      expect(a).to.have.length(3);
-      expect(a[0]).to.equal(8);
-      expect(a[1]).to.equal(9);
-      expect(a[2]).to.equal(10);
-    });
-  });
-  describe('#capitalize', () => {
-    it('capitalizes the first character of a word', () => {
-      expect(capitalize('hello')).to.equal('Hello');
-      expect(capitalize('Hello')).to.equal('Hello');
-      expect(capitalize('hello world')).to.equal('Hello world');
+  describe('Object', () => {
+    describe('#assign', () => {
+      it('merges the sources into the target', () => {
+        expect(assign({foo:'bar'},{foo:'baz'})).to.have.property('foo', 'baz');
+        const a = assign({foo:'bar'},{foo:'baz',hello:'world'})
+        expect(a).to.have.property('foo', 'baz');
+        expect(a).to.have.property('hello', 'world');
+      });
     });
   });
 });
--- src/util.js
+++ src/util.js
@@ -72,14 +72,43 @@ function capitalize(string) {
   return string && (string.charAt(0).toUpperCase() + string.slice(1));
 }
 
+/**
+ * Merge sources into target
+ * 
+ * @param {object} target
+ * @param {arguments} soutces
+ * @return {object}
+ */
+function assign(target, sources) {
+  if (target == null) {
+    throw new TypeError('Object.assign target cannot be null or undefined');
+  }
+  const to = Object(target);
+  const hasOwnProperty = Object.prototype.hasOwnProperty;
+  for (var nextIndex = 1; nextIndex < arguments.length; nextIndex++) {
+    var nextSource = arguments[nextIndex];
+    if (nextSource == null) {
+      continue;
+    }
+    var from = Object(nextSource);
+    for (var key in from) {
+      if (hasOwnProperty.call(from, key)) {
+        to[key] = from[key];
+      }
+    }
+  }
+  return to;
+}
+
 module.exports = {
-
+  // Misx
   getVendorPrefix,
-
+  // Array
   dropRight,
   last,
   takeRight,
-
-  capitalize
-
+  // String
+  capitalize,
+  // Object
+  assign
 };
